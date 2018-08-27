76e88c5f77118d2fe64f0c211829df9c1fe989bb
theAlex <info@thealex.ru>
2018-08-27 16:06:51 +0300

    xxx

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        $this->call(DatabaseFromDumpSeeder::class);
+        //$this->call(DatabaseFromDumpSeeder::class);
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
     }
